56080de58fa73d639191d04e3809918b39050ee7
yanboyuan <yanboyuan@sensetime.com>
2018-03-05 13:09:10 +0800

    调整参数

--- GRWA/test_5wave/cuda-a2c-simplestnet_one_service.sh
+++ GRWA/test_5wave/cuda-a2c-simplestnet_one_service.sh
@@ -10,7 +10,7 @@
 #SBATCH -w BJ-IDC1-10-10-16-81
 
 srun python -u ../main.py --mode learning --cnn simplestnet --num-steps 16 --cuda True --step-over one_service \
---net 6node.md --wave-num 10 --rou 8 --miu 120 --max-iter 1000 \
+--net 6node.md --wave-num 5 --rou 8 --miu 120 --max-iter 1000 \
 --k 1 --weight None --workers 16 --steps 10e6 \
 --img-height 224 --img-width 224
 
